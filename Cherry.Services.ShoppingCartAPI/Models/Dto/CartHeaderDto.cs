f748a291fbbcb63ef9acc7e0f24b4887a8408aff
Shivansh Srivastava <srishivansh2021@gmail.com>
2024-10-20 13:54:45 +0530

    Added Product API, Shopping Cart APi and Created Home Page

---
+++ Cherry.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cherry.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CartHeaderDto
+    {
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+       
+        public double Discount { get; set; }
+      
+        public double CartTotal { get; set; }
+    }
+}
---
+++ Cherry.Services.ShoppingCartAPI/Service/IService/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using Cherry.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Cherry.Services.ShoppingCartAPI.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<CouponDto> GetCoupon(string couponCode);
+    }
+}
