ff299f67f4c28f49d1699af67f756f99007af249
Shivansh Srivastava <srishivansh2019@gmail.com>
2024-11-30 15:24:11 +0530

    back-to-original

--- Cherry.MessageBus/MessageBus.cs
+++ Cherry.MessageBus/MessageBus.cs
@@ -11,14 +11,11 @@ using System.Threading.Tasks;
 
 namespace Cherry.MessageBus
 {
-    //hello
     public class MessageBus : IMessageBus
     {
         private string connectionString = "Endpoint=sb://cherryweb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=CN0HnFwXobc2/k+DMUzcJVFWl7fwVSuGt+ASbM3gHT8=";
         public async Task PublishMessage(object message,string topic_queue_Name)
         {
-            var configuration = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory).AddJsonFile("appSettings.json").Build();
-            Console.WriteLine(connectionString); 
             await using var client = new ServiceBusClient(connectionString);
             ServiceBusSender sender=client.CreateSender(topic_queue_Name);
             var jsonMessage=JsonConvert.SerializeObject(message);
