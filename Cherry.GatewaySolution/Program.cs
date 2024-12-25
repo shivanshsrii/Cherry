1ccbb516ac910012e1182e69697a08d7b9a8d4bd
Shivansh Srivastava <srishivansh2019@gmail.com>
2024-12-25 11:48:21 +0530

    Fully_functional_Web_Hosted_On_Azure_Free_trial

--- Cherry.GatewaySolution/Program.cs
+++ Cherry.GatewaySolution/Program.cs
@@ -5,21 +5,19 @@ using Ocelot.Values;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddAppAuthetication();
-builder.Configuration.AddJsonFile("ocelot.json",optional:false,reloadOnChange:true);    
+if (builder.Environment.EnvironmentName.ToString().ToLower().Equals("production"))
+{
+    builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
+}
+else
+{
+    builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+}
+
 builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
-
-
-
-//if (builder.Environment.EnvironmentName.ToString().ToLower().Equals("production"))
-//{
-//    builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
-//}
-//else
-//{
-//    builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-//}
