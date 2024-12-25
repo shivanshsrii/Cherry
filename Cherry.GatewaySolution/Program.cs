4951988e994a619f1d932c42fe4f52b0ad51bab4
Shivansh Srivastava <srishivansh2019@gmail.com>
2024-12-25 12:59:51 +0530

    conlict resolved

--- Cherry.GatewaySolution/Program.cs
+++ Cherry.GatewaySolution/Program.cs
@@ -5,7 +5,7 @@ using Ocelot.Values;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddAppAuthetication();
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile("ocelot.json",optional:false,reloadOnChange:true);
 builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 
